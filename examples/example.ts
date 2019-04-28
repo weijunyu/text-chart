3d52c540ea25ceac7ef9f72f98b8f816c606a5c7
Wei Junyu <junyuwei@gmail.com>
2019-04-28 23:47:19 +0800

    Updated readme and examples

--- examples/example.ts
+++ examples/example.ts
@@ -3,38 +3,60 @@ import { BarCharacters } from "../interfaces";
 
 const barChartNonScaled = new BarChart();
 console.log("Non-scaled bar chart:");
-console.log(barChartNonScaled.setData([
-    ["apples", 15],
-    ["oranges", 3],
-    ["bananas", 12],
-]).render());
+console.log(barChartNonScaled
+    .setData([
+        ["apples", 15],
+        ["oranges", 3],
+        ["bananas", 12],
+    ])
+    .render());
 
 const barChartScaled = new BarChart();
 console.log("Scaled bar chart:");
-console.log(barChartScaled.setProperties({
-    barCharacter: BarCharacters.WhiteSquare,
-    width: 10,
-}).setData([
-    ["apples", 11],
-    ["oranges", 33],
-    ["strawberries", 111],
-]).render());
+console.log(barChartScaled
+    .setProperties({
+        barCharacter: BarCharacters.WhiteSquare,
+        width: 10,
+    })
+    .setData([
+        ["apples", 15],
+        ["oranges", 32],
+        ["strawberries", 98],
+    ])
+    .render());
+
+const histogramDefault = new Histogram();
+const histogramDefaultData = [];
+for (let i = 0; i < 100; i++) {
+    histogramDefaultData.push(Math.random() * 100);
+}
+console.log('Default histogram:');
+console.log(histogramDefault
+    .setData(histogramDefaultData)
+    .render());
 
 const histogramScaled = new Histogram();
 const histogramData = [];
 for (let i = 0; i < 1000; i++) {
     histogramData.push(Math.random() * 1000);
 }
-console.log(histogramScaled.setProperties({
-    barCharacter: BarCharacters.BlackSquare,
-    width: 20,
-}).setData(histogramData).render());
+console.log('Scaled histogram:');
+console.log(histogramScaled
+    .setProperties({
+        width: 20,
+    })
+    .setData(histogramData)
+    .render());
 
 const histogramSmallScale = new Histogram();
 const histogramSmallData = [];
-for (let i = 0; i < 1000; ++i) {
+for (let i = 0; i < 10000; ++i) {
     histogramSmallData.push(Math.random());
 }
-console.log(histogramSmallScale.setProperties({
-    width: 20,
-}).setData(histogramSmallData).render());
+console.log("Small scale histogram:")
+console.log(histogramSmallScale
+    .setProperties({
+        width: 20,
+    })
+    .setData(histogramSmallData)
+    .render());
--- readme.md
+++ readme.md
@@ -1,16 +1,12 @@
 # Text Charts
 
-## Histogram label rounding
-If interval >= 1
+## Default histogram label rounding
+### Interval >= 1
 Round non-integer labels to nearest 2 decimal places
 
-| Interval | log(interval) | round to nearest decimal places |
+### Interval < 1
+| interval | log(interval) | round to nearest decimal places |
 | --- | --- | --- |
 | 0.99 to 0.1 | 0 to -1 | 2 |
 | 0.099 to 0.01 | -1 to -2 | 3 |
 | ... | -(n - 1) to -n | n + 1 |
-
-```
-n = Math.floor(log(interval))
-label = roundToDecimal(interval, n + 1)
-```
