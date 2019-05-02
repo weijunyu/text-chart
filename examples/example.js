18aa53ffcb6bdc6d4c4a43959a0fb163cfc3a1fe
Wei Junyu <junyuwei@gmail.com>
2019-05-02 22:46:54 +0800

    Updated js examples

--- examples/example.js
+++ examples/example.js
@@ -1,2 +1,36 @@
 const TextChart = require("../dist/index");
-console.log(TextChart);
+const barChartNonScaled = new TextChart.BarChart();
+console.log("Non-scaled bar chart:");
+console.log(
+    barChartNonScaled
+        .setData([["apples", 15], ["oranges", 3], ["bananas", 12]])
+        .render()
+);
+
+console.log(
+    barChartNonScaled
+        .setData([["apples", 15], ["oranges", 3], ["bananas", 12]])
+        .setProperties({
+            width: 10,
+            barCharacter: TextChart.BarCharacters.WhiteSquare
+        })
+        .render()
+);
+
+const barChartScaled = new TextChart.BarChart();
+console.log(
+    barChartScaled
+        .setProperties({
+            barCharacter: TextChart.BarCharacters.WhiteSquare,
+            width: 10
+        })
+        .setData([["apples", 15], ["oranges", 32], ["strawberries", 98]])
+        .render()
+);
+
+const histogramDefault = new TextChart.Histogram();
+const histogramDefaultData = [];
+for (let i = 0; i < 100; i++) {
+    histogramDefaultData.push(Math.random() * 100);
+}
+console.log(histogramDefault.setData(histogramDefaultData).render());
